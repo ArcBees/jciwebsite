8f88503078bc9e913650a4082e69ff049c3af527
Simon-Pierre Gingras <simon.pierre.gingras@arcbees.com>
2013-01-24 23:01:28 -0500

    Yeah bitch

--- src/main/java/com/jci/client/resource/main/MainResource.java
+++ src/main/java/com/jci/client/resource/main/MainResource.java
@@ -2,10 +2,28 @@ package com.jci.client.resource.main;
 
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.resources.client.ImageResource;
 
 public interface MainResource extends ClientBundle {
     public interface Style extends CssResource {
+        String dragUp();
+
+        String sliderWrapper();
+
+        String sponsorWrapper();
+
+        String sliderPhoto();
+
+        String sliderButtons();
+
+        String aWonderfulCity();
     }
 
     Style style();
+
+    ImageResource homeImage001();
+
+    @ImageResource.ImageOptions(repeatStyle = ImageResource.RepeatStyle.Horizontal)
+    ImageResource degrade_bt();
+
 }
