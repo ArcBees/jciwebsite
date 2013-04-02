f4645b024cc8bbb288dcbd906753cb6a79a3fa0f
Alexandre Breton <alexandre.breton@arcbees.com>
2013-04-02 09:42:13 -0400

    Create Contact Page

---
+++ src/main/java/com/jci/client/application/contact/ContactPageView.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2013 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.jci.client.application.contact;
+
+import com.arcbees.core.client.mvp.ViewImpl;
+import com.google.gwt.dom.client.DivElement;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.SpanElement;
+import com.google.gwt.event.logical.shared.AttachEvent;
+import com.google.gwt.query.client.Function;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Widget;
+import com.jci.client.resource.CommonResource;
+import com.jci.client.resource.program.ProgramResource;
+
+import javax.inject.Inject;
+
+import static com.google.gwt.query.client.GQuery.$;
+
+public class ContactPageView extends ViewImpl implements ContactPagePresenter.MyView{
+    interface Binder extends UiBinder<Widget, ContactPageView> {
+    }
+
+    @Inject
+    public ContactPageView(Binder uiBinder) {
+        initWidget(uiBinder.createAndBindUi(this));
+    }
+}
